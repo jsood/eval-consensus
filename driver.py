3bbd9ea85e944d9181c5caa690a633ad6667bd8f
dhawanradhika <32319235+dhawanradhika@users.noreply.github.com>
2018-11-17 14:42:24 -0500

    Create driver.py
    Basic Skeleton

---
+++ driver.py
@@ -0,0 +1,17 @@
+class Driver: 
+	def calc_elapsed_time():
+			# end_time - start_time
+
+	def calc_cpu_time():
+			# end_ctime - start_ctime
+	
+	def calc_memory():
+	
+	def receive_start_statistics():
+			# start_time, start_ctime
+	
+	def receive_end_statistics():
+			# end_time, end_ctime
+			
+	def main():
+			# n_proposers, n_learners, n_acceptors, repetitions
